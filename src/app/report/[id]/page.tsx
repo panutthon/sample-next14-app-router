3dd65418a42c64727762ddd9ccd4e3ca422a153b
panutthon <93002896+panutthon@users.noreply.github.com>
2024-05-16 10:24:37 +0700

    useParams, useSearchParams

--- src/app/report/[id]/page.tsx
+++ src/app/report/[id]/page.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+"use client";
+
+import { useParams, useSearchParams } from "next/navigation";
+import React from "react";
 
 type Props = {
-    params: {
-        id: string;
-    }
-    
-}
+  params: {
+    id: string;
+  };
+};
 
-function Report({params}: Props) {
+function Report({}: Props) {
+  const params = useParams(); // อ่านค่า parameter จาก URL
+
+  // อ่านค่า query จาก URL
+  const query = useSearchParams(); // get query from URL
   return (
-    <div>Report id: { params.id }</div>
-  )
+    <div>
+      <p>Report id: {params.id}</p>
+      <p>Query search : {query.get("search")}</p>
+      <p>Query cat : {query.get("cat")}</p>
+    </div>
+  );
 }
 
-export default Report
+export default Report;
